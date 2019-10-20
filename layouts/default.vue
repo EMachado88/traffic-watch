a11c1d678e2e14d1056197e8aebbda40122dd195
Emanuel Machado <emachado@emachado.pt>
2019-10-20 21:21:53 +0100

    Added export funcionality

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,9 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
     </v-app-bar>
     <v-content>
@@ -37,7 +11,7 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-bottom-navigation>
+    <v-bottom-navigation fixed>
       <v-btn value="counter">
         <span>Counter</span>
         <v-icon>mdi-counter</v-icon>
@@ -47,11 +21,6 @@
         <span>Settings</span>
         <v-icon>mdi-wrench</v-icon>
       </v-btn>
-
-      <v-btn color="success" class="white--text" value="nearby">
-        <span>Nearby</span>
-        <v-icon>mdi-map-marker</v-icon>
-      </v-btn>
     </v-bottom-navigation>
   </v-app>
 </template>
@@ -60,18 +29,6 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
       title: 'Traffic Watch'
     }
   }
--- pages/index.vue
+++ pages/index.vue
@@ -7,27 +7,65 @@
     <v-btn
       v-for="(counter, index) in counters"
       :key="index"
-      class="pa-5 mt-8 d-flex justify-space-between title"
+      class="pa-5 mt-8 d-flex justify-space-between display-1 font-weight-bold"
       block
       color="secondary"
       x-large
       dark
       style="height: auto"
-      @click.end="click(counter)"
+      @click.end="click(counter, true)"
     >
-      <v-icon>
+      <v-icon x-large>
         mdi-{{ counter.icon }}
       </v-icon>
       <span>{{ counter.name }}</span>
       <span>{{ counter.total }}</span>
     </v-btn>
+
+    <v-fab-transition>
+      <v-btn
+        v-if="startTime"
+        color="success"
+        fab
+        large
+        dark
+        bottom
+        right
+        style="z-index: 100;
+              right: 7px;
+              bottom: 63px;
+              position: fixed;"
+        @click.end="save()"
+      >
+        <v-icon>mdi-download</v-icon>
+      </v-btn>
+    </v-fab-transition>
+
+    <v-bottom-sheet v-model="resetSheet">
+      <v-sheet class="text-center pa-8">
+        <p>
+          Reset the counter?
+        </p>
+        <v-btn
+          color="success"
+          @click="reset()"
+        >
+          RESET
+        </v-btn>
+      </v-sheet>
+    </v-bottom-sheet>
   </v-layout>
 </template>
 
 <script>
+import { saveAs } from 'file-saver'
+
 export default {
   data () {
     return {
+      resetSheet: false,
+      startTime: false,
+      username: 'Jules',
       counters: [
         {
           name: 'Cars',
@@ -43,8 +81,59 @@ export default {
     }
   },
   methods: {
-    click (counter) {
-      counter.total++
+    click (counter, increment) {
+      if (!this.startTime) {
+        this.startTime = new Date()
+      }
+      increment ? counter.total++ : counter.total--
+    },
+    save () {
+      const endTime = new Date()
+      const date = this.startTime.toISOString().split('T')[0]
+      let text =
+`Traffic Watch
+
+User:     ${this.username}
+Date:     ${date}
+Start:    ${this.startTime.toLocaleTimeString()}
+End:      ${endTime.toLocaleTimeString()}
+
+
+`
+      let maxNameLength = 0
+      for (const counter of this.counters) {
+        if (counter.name.length > maxNameLength) {
+          maxNameLength = counter.name.length
+        }
+      }
+
+      let total = 0
+      for (const counter of this.counters) {
+        let name = `${counter.name}:`
+        while (name.length < (maxNameLength + 1)) {
+          name = `${name} `
+        }
+        text += `${name.replace('_', ' ')}          ${counter.total}\n`
+        total += counter.total
+      }
+
+      let totalText = 'TOTAL:'
+      while (totalText.length < (maxNameLength + 1)) {
+        totalText = `${totalText} `
+      }
+      text += `${totalText.replace('_', ' ')}          ${total}\n`
+
+      const blob = new Blob([text], { type: 'text/plain;charset=utf-8' })
+      saveAs(blob, `Traffic Watch-${this.username}-${date}`)
+
+      this.resetSheet = true
+    },
+    reset () {
+      this.resetSheet = false
+      for (const counter of this.counters) {
+        counter.total = 0
+      }
+      this.startTime = false
     }
   }
 }
